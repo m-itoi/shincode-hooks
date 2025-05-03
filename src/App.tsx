44cafae97463394122e9a0e71593ee0dbf2d99ed
m-itoi <m-itoi@b-tm.co.jp>
2025-05-03 10:40:36 +0900

    useState 使用前

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css'
 
+
 function App() {
+  let count = 0;
+
+  const handleClick = () => {
+    count++;
+    console.log(count);
+  };
+
   return (
     <div className="App">
+      <h1>useState</h1>
+      <p>Count: {count}</p>
+      {/* countは実際増えているが画面には反映されない */}
+      <button onClick={handleClick}>＋</button>
     </div>
   );
 };
