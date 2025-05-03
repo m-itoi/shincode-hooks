303353e46a30495f1a48ec367d1ef8dca9e0fabd
m-itoi <m-itoi@b-tm.co.jp>
2025-05-03 10:50:01 +0900

    useState 実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
-
+import { useState } from 'react';
 
 function App() {
-  let count = 0;
+  // let count = 0;
+  const [count, setCount] = useState(0);
 
   const handleClick = () => {
-    count++;
-    console.log(count);
+    setCount(count + 1);
+    console.log(count); // ↓
+    // クイズ：初回クリック後にconsole出力される値は？
+    // こたえ：1回目のクリック時は0。（2回目のクリック時は1、3回目のクリック時は2）
+    // クリック"した時"のcountの値になる。setCount後の値は反映されない。
+    // setCountは非同期処理なので、すぐに反映されないから。
+    // レンダリング後に反映される。
   };
 
   return (
@@ -14,6 +20,7 @@ function App() {
       <h1>useState</h1>
       <p>Count: {count}</p>
       {/* countは実際増えているが画面には反映されない */}
+      {/* setCountを使うと反映される。useStateを使うとレンダリングされるから。 */}
       <button onClick={handleClick}>＋</button>
     </div>
   );
