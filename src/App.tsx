76307e94590c166d0d6808209f3e97b9c0a1ef54
m-itoi <m-itoi@b-tm.co.jp>
2025-05-03 12:19:30 +0900

    useMemo メモ化

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,14 @@ function App() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const square = () => {
+  // メモ化することで仮想DOMのための再計算を防げる
+  const square = useMemo(() => {
     let i = 0;
     while (i < 1000000000) {
       i++;
     }
     return count2 * count2;
-  };
+  }, [count2]);
 
   return (
     <div className="App">
@@ -57,7 +58,7 @@ function App() {
       <h1>useMemo</h1>
       <div>カウント１：{count1}</div>
       <div>カウント２：{count2}</div>
-      <div>square結果：{square()}</div>
+      <div>square結果：{square}</div>
       <button onClick={()=> setCount1(count1 +1)}>カウント１</button>
       <button onClick={()=> setCount2(count2 +1)}>カウント２</button>
     </div>
